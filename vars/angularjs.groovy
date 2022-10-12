0361134d577685122c11591b439b33baa77c1d40
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-10-11 20:05:02 -0700

    update

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -1,49 +1,76 @@
-def lintCheck() {
-    sh ''' 
-         echo Starting lint checks ${COMPONENT}
-         echo Lint Checks Completed for ${COMPONENT}
-       
-       ''' 
-}
-
 def call() {
-    pipeline {
-        agent any 
-        stages {
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            }
-            stage('Sonar Check') {
-                steps {
-                    script { 
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarCheck()
-                    }
-                }
-            }
-           stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                    steps {
-                        sh 'echo Unit Test Cases Completed'
-                         }
-                    }
-                stage('Integration Tests') {
-                    steps {
-                        sh 'echo Integration Test Cases Completed'
-                         }
-                    }
-                stage('Functional Tests') {
-                    steps {
-                        sh 'echo Functional Test Cases Completed'
-                         }
-                    }
-                }
-            }
-        }    // end of statges 
+    node {
+        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
+        env.APPTYPE="angularjs"
+        common.sonarCheck()
+        common.lintCheck()
+        env.ARGS="-Dsonar.sources=."
+        common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifact()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// def lintCheck() {
+//     sh ''' 
+//          echo Starting lint checks ${COMPONENT}
+//          echo Lint Checks Completed for ${COMPONENT}
+       
+//        ''' 
+// }
+
+// def call() {
+//     pipeline {
+//         agent any 
+//         stages {
+//             stage('Lint Check') {
+//                 steps {
+//                     script { 
+//                         lintCheck()
+//                     }
+//                 }
+//             }
+//             stage('Sonar Check') {
+//                 steps {
+//                     script { 
+//                         env.ARGS="-Dsonar.sources=."
+//                         common.sonarCheck()
+//                     }
+//                 }
+//             }
+//            stage('Test Cases') {
+//             parallel {
+//                 stage('Unit Tests') {
+//                     steps {
+//                         sh 'echo Unit Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Integration Tests') {
+//                     steps {
+//                         sh 'echo Integration Test Cases Completed'
+//                          }
+//                     }
+//                 stage('Functional Tests') {
+//                     steps {
+//                         sh 'echo Functional Test Cases Completed'
+//                          }
+//                     }
+//                 }
+//             }
+//         }    // end of statges 
+//     }
+// }
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,83 +1,82 @@
-def lintCheck() {
-    sh ''' 
-         # We want Devs to handle the lint checks failure 
-         # npm i jslint 
-         # node_modules/jslint/bin/jslint.js  server.js || true 
-         echo Starting lint checks
-         echo Lint Checks Completed for ${COMPONENT}
-    ''' 
-}
-
 def call() {
-    pipeline {
-        agent any 
-    environment {
-        SONAR      = credentials('SONAR')
-    }
- 
-        stages {
-            stage('Downloading the dependencies') {
-                steps {
-                    sh "npm install"
-                }
-            }
-
-            stage('Lint Check') {
-                steps {
-                    script { 
-                        lintCheck()
-                    }
-                }
-            }
-
-            stage('Sonar Check') {
-                steps {
-                    script { 
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarCheck()
-                    }
-                }
-            }
-
-           stage('Test Cases') {
-            parallel {
-                stage('Unit Tests') {
-                    steps {
-                        sh 'echo Unit Test Cases Completed'
-                         }
-                    }
-                stage('Integration Tests') {
-                    steps {
-                        sh 'echo Integration Test Cases Completed'
-                         }
-                    }
-                stage('Functional Tests') {
-                    steps {
-                        sh 'echo Functional Test Cases Completed'
-                         }
-                    }
-                }
-            }
-
-            stage('Prepare Artifacts') {
-                when {
-                    expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
-                }
-                steps {
-                    echo 'echo'
-                }
-            }
-
-            stage('Upload Artifacts') {
-                when {
-                    expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
-                }
-                steps {
-                    echo 'echo'
-                }
-            }
-        }    // end of statges 
+    node {
+        git branch: 'main', url: "https://github.com/b50-clouddevops/${COMPONENT}.git"
+        env.APPTYPE="nodejs"
+        common.sonarCheck()
+        common.lintCheck()
+        env.ARGS="-Dsonar.sources=."
+        common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifact()
+        }
     }
 }
+
+// def call() {
+//     pipeline {
+//         agent any 
+//         environment {
+//             SONAR      = credentials('SONAR')
+//             NEXUS      = credentials('NEXUS')
+//         }
+ 
+//         stages {
+//             stage('Downloading the dependencies') {
+//                 steps {
+//                     sh "npm install"
+//                 }
+//             }
+
+
+//             stage('Sonar Check') {
+//                 steps {
+//                     script { 
+
+//                     }
+//                 }
+//             }
+
+
+
+//             stage('Check the release') {
+//                 when {
+//                     expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
+//                 }
+//                 steps {
+//                     script {
+//                         env.UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -s http://172.31.0.75:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true')
+//                         print UPLOAD_STATUS
+//                     }
+//                 }
+//             }
+
+//             stage('Prepare Artifacts') {
+//                 when {
+//                     expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
+//                     expression { env.UPLOAD_STATUS == "" }
+//                 }
+//                 steps {
+//                     sh ''' 
+//                         npm install 
+//                         zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js
+
+//                     ''' 
+//                 }
+//             }
+
+//             stage('Upload Artifacts') {
+//                 when {
+//                     expression { env.TAG_NAME != null }   // Only runs when you run this against the TAG
+//                     expression { env.UPLOAD_STATUS == "" }
+//                 }
+//                 steps {
+//                     sh ''' 
+//                         curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.0.75:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+//                     '''
+//                 }
+//             }
+//         }    // end of statges 
+//     }
+// }
 
 // call is the default function which will be called when you call the fileName 
